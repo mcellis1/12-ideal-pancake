1808fef7934d3606a3a685233496f50feb46c1a7
napling <matthewcellis98@gmail.com>
2024-12-06 22:57:52 -0600

    cann get data frrom database nnow

--- main/server.js
+++ main/server.js
@@ -1,5 +1,8 @@
-const express = require('express')
-const { Pool } = require('pg')
+import express from 'express'
+import inquirer from 'inquirer'
+import pg from 'pg'
+
+const { Pool } = pg
 const PORT = process.env.PORT || 3001
 const app = express()
 
@@ -8,23 +11,76 @@ app.use(express.json())
 
 const pool = new Pool(
     {
+        host: 'localhost',
         user: 'postgres',
         password: '1612',
-        host: 'localhost',
         database: 'employee_db'
     },
     console.log('connected to employee_db')
 )
 
-pool.connect()
+const client = await pool.connect()
 
-app.get()
+const init = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'init',
+            message: 'what would you like to do?',
+            choices: [
+                'view all departments',
+                'view all roles',
+                'view all employees',
+                'add a department',
+                'add a role',
+                'add an employee',
+                'update an employee role',
+                'exit'
+            ]
+        }
+    ]).then((answer) => {
+        if (answer.init === 'view all departments') {
+            getDepartments()
+        } else if (answer.init === 'view all roles') {
+            getRoles()
+        } else if (answer.init === 'view all employees') {
+            getEmployees()
+        } else if (answer.init === 'add a department') {
+            addDepartment()
+        } else if (answer.init === 'exit') {
+            process.exit(0)
+        }
+    })
+}
 
-app.post()
+const getDepartments = async() => {
+    const sql = `SELECT departments.id, departments.department_name FROM departments;`
+    const res = await client.query(sql)
+    console.table(res.rows)
+    init()
+}
 
-app.put()
+const getRoles = async () => {
+    const sql = `SELECT roles.id, roles.title, roles.salary, departments.department_name FROM roles INNER JOIN departments ON (departments.id = roles.department_id);`
+    const res = await client.query(sql)
+    console.table(res.rows)
+    init()
+}
 
-app.delete()
+const getEmployees = async () => {
+    const sql = `SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.department_name, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employees LEFT JOIN employees manager on manager.id = employees.manager_id INNER JOIN roles ON (roles.id = employees.role_id) INNER JOIN departments ON (departments.id = roles.department_id) ORDER BY employees.last_name;`
+    const res = await client.query(sql)
+    console.table(res.rows)
+    init()
+}
+
+const addDepartment = () => {}
+
+const addRole = () => {}
+
+const addEmployee = () => {}
+
+const updateEmployeeRole = () => {}
 
 app.use((req, res) => {
     res.status(404).end()
@@ -33,3 +89,5 @@ app.use((req, res) => {
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
 })
+
+init()
