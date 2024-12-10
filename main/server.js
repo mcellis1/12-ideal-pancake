1eba6f5d9178936f8388b8077451db077c7bd11b
napling <matthewcellis98@gmail.com>
2024-12-10 00:12:33 -0600

    added options to add role and update employee

--- main/server.js
+++ main/server.js
@@ -40,19 +40,15 @@ const init = () => {
         }
     ])
         .then((answer) => {
-            if (answer.init === 'view all departments') {
-                getDepartments()
-            } else if (answer.init === 'view all roles') {
-                getRoles()
-            } else if (answer.init === 'view all employees') {
-                getEmployees()
-            } else if (answer.init === 'add a department') {
-                addDepartment()
-            } else if (answer.init === 'add a role') {
-                addRole()
-            } else if (answer.init === 'exit') {
-                process.exit(0)
-            }
+            if (answer.init === 'view all departments') { getDepartments() }
+            else if (answer.init === 'view all roles') { getRoles() }
+            else if (answer.init === 'view all employees') { getEmployees() }
+            else if (answer.init === 'add a department') { addDepartment() }
+            else if (answer.init === 'add a role') { addRole() }
+            else if (answer.init === 'add an employee') { addEmployee() }
+            else if (answer.init === 'update an employee role') { updateEmployeeRole() }
+            else if (answer.init === 'exit') { process.exit(0) }
+            else { return }
         })
 }
 
@@ -96,39 +92,125 @@ const addDepartment = () => {
 const addRole = async () => {
     const sql = `SELECT * FROM departments;`
     const res = await client.query(sql)
-    console.log(res.rows)
-    // departmentList.map(departments => ({
-    //     name: departments.department_name,
-    //     value: departments.id
-    // }))
-    // return inquirer.prompt([
-    //     {
-    //         type: 'input',
-    //         name: 'title',
-    //         message: 'what is the name of the role?'
-    //     },
-    //     {
-    //         type: 'input',
-    //         name: 'salary',
-    //         message: 'what is the salary for this role?'
-    //     },
-    //     {
-    //         type: 'list',
-    //         name: 'department',
-    //         message: 'which department does this role belong to?'
-    //     }
-    // ])
-    //     .then((answers) => {
-    //         const sql = `INSERT INTO roles(title, salary, department_id) VALUES('${answers.title}', '${answers.salary}', '${answers.department}')`
-    //         client.query(sql)
-    //         console.log(`added ${answers.title} as a new role to the database`)
-    //         init()
-    //     })
+    const departmentList = res.rows.map(departments => ({
+        name: departments.department_name,
+        value: departments.id
+    }))
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'what is the name of the role?'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'what is the salary for this role?'
+        },
+        {
+            type: 'list',
+            name: 'department',
+            message: 'which department does this role belong to?',
+            choices: departmentList
+        }
+    ])
+        .then((answers) => {
+            const sql = `INSERT INTO roles(title, salary, department_id) VALUES('${answers.title}', '${answers.salary}', ${answers.department});`
+            client.query(sql)
+            console.log(`added ${answers.title} as a new role to the database`)
+            init()
+        })
 }
 
-const addEmployee = () => { }
+const addEmployee = async () => {
+    const sqlEmp = `SELECT * FROM employees;`
+    const resEmp = await client.query(sqlEmp)
+    const employeeList = resEmp.rows.map(employees => ({
+        name: employees.first_name.concat(' ', employees.last_name),
+        value: employees.id
+    }))
 
-const updateEmployeeRole = () => { }
+    const sqlRole = `SELECT * FROM roles;`
+    const resRole = await client.query(sqlRole)
+    const roleList = resRole.rows.map(roles => ({
+        name: roles.title,
+        value: roles.id
+    }))
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'what is the first name of the employee?'
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'what is the last name of the employee?'
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'what is the role of the employee?',
+            choices: roleList
+        },
+        {
+            type: 'list',
+            name: 'manager',
+            message: 'what manager does the employee report to?',
+            choices: employeeList
+        }
+    ])
+        .then((answers) => {
+            const sql = `INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES('${answers.first_name}', '${answers.last_name}', ${answers.role}, ${answers.manager});`
+            client.query(sql)
+            console.log(`added ${answers.first_name.concat(' ', answers.last_name)} as a new role to the database`)
+            init()
+        })
+}
+
+const updateEmployeeRole = async () => {
+    const sqlEmp = `SELECT * FROM employees;`
+    const resEmp = await client.query(sqlEmp)
+    const employeeList = resEmp.rows.map(employees => ({
+        name: employees.first_name.concat(' ', employees.last_name),
+        value: employees.id
+    }))
+
+    const sqlRole = `SELECT * FROM roles;`
+    const resRole = await client.query(sqlRole)
+    const roleList = resRole.rows.map(roles => ({
+        name: roles.title,
+        value: roles.id
+    }))
+
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employee',
+            message: 'which employee role do you want to update?',
+            choices: employeeList
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'what is the new role for the employee?',
+            choices: roleList
+        },
+        {
+            type: 'list',
+            name: 'manager',
+            message: 'who is the new manager for the employee?',
+            choices: employeeList
+        }
+    ])
+        .then((answers) => {
+            const sql = `UPDATE employees SET role_id=${answers.role}, manager_id=${answers.manager} WHERE id=${answers.employee};`
+            client.query(sql)
+            console.log(`updated employee role and manager in the database`)
+            init()
+        })
+}
 
 app.use((req, res) => {
     res.status(404).end()
